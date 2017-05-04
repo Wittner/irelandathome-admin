566284af9cb267c1774f1fa8722a5d90cc4aba6c
Mike Brady <mike@irelandathome.com>
2017-05-04 15:15:00 +0100

    Vat percentage chooser on edit_booking view

--- igsys/application/controllers/booking.php
+++ igsys/application/controllers/booking.php
@@ -433,7 +433,7 @@ class Booking extends Controller
 		$agentFee = $bookingFee + $commissionAmount;
 		$ownerBalance = $commissionableCost - $commissionAmount - $vatAmount + $ownerTotalCharges;
 
-		echo "owner balance: " . $ownerBalance + ", vat rate: " . $vatPercentage . ", vat amount: " . $vatAmount . ", owner balance: " . $ownerBalance;
+		echo "owner balance: " . $ownerBalance . ", vat rate: " . $vatPercentage . ", vat amount: " . $vatAmount . ", owner balance: " . $ownerBalance;
 
 		$ownerPaid = $this->input->post('ownerPaid');
 		$ownerPaidDate = $this->input->post('ownerPaidDate');
